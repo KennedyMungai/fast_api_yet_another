a8cf6994a1ca47b54138911fb5fdca87c6affcc5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-13 14:50:25 +0300

    Wrote the login_user function which holds the logic for the login endpoint

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """The entry point to the program"""
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status, Security
 from sqlalchemy import orm
 
 from schemas import UserRequest
-from services import get_db, get_user_by_email, create_user, create_token
+from services import get_db, get_user_by_email, create_user, create_token, login
 
 app = FastAPI()
 
@@ -30,5 +30,28 @@ async def register_user(_user: UserRequest, _db: orm.Session = Depends(get_db)):
                             detail="The Email address provided is already in use")
 
     db_user = await create_user(_user, _db)
+
+    return await create_token(db_user)
+
+
+@app.post("/api/v1/login")
+async def login_user(form_data: OAuth2PasswordRequestForm = Depends(), db: orm.Session = Depends(get_db)):
+    """Defined the function for the login endpoint
+
+    Args:
+        form_data (OAuth2PasswordRequestForm, optional): Contains the email and the password. Defaults to Depends().
+        db (orm.Session, optional): The database sessoion. Defaults to Depends(get_db).
+
+    Raises:
+        HTTPException: An unauthorized exception is raised incase of false login credentials
+
+    Returns:
+        Token: Creates and returns a token for the user
+    """
+    db_user = await login(form_data.username, form_data.password, db)
+
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong login credentials")
 
     return await create_token(db_user)
