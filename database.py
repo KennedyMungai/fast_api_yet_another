8b506b38b9905755b50b056f4f89430d7a3530ff
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-13 03:18:37 +0300

    Created an engine for sqlite

--- database.py
+++ database.py
@@ -2,6 +2,9 @@
 import sqlalchemy
 import sqlalchemy.ext.declarative
 import sqlalchemy.orm
+from sqlalchemy import create_engine
 
 
 DB_URL = "sqlite:///.dbfile.db"
+
+engine = create_engine(DB_URL, connect_args={"check_same_thread"})
