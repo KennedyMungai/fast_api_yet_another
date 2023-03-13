8257bfd715edcef111bc3b1ad256d698f63f83ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-13 11:36:04 +0300

    Defined the logic of the user registration endpoint

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 """The entry point to the program"""
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi import security
 from sqlalchemy import orm
+from schemas import UserRequest, UserResponse
+from services import get_db
 
 app = FastAPI()
+
+
+@app.post("/api/v1/users")
+async def register_user(user: UserRequest, _db: orm.Session = Depends(get_db())):
+    pass
