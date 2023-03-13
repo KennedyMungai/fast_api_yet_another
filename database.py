9ff99aff6ed9eb808615b270ac021325575aff18
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-13 03:28:47 +0300

    Readded the connect_args parameter and passed in the check_same_thread with a value of False

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 
 DB_URL = "sqlite:///.dbfile.db"
 
-engine = create_engine(DB_URL)
+engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
 
 session_local = sessionmaker(
     autocommit=False, autoflush=False, bind=engine)
