84c8d3b17c3e5e51196e1ac97fb1791b01664edd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-13 03:21:31 +0300

    Created a database session

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
 """The database configuration file"""
 import sqlalchemy
 import sqlalchemy.ext.declarative
-import sqlalchemy.orm
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 
 DB_URL = "sqlite:///.dbfile.db"
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread"})
+
+session_local = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
