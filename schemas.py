235f9061b39d9008d2ae1110f2e727e5377d5c4d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-13 04:41:27 +0300

    Created the UserBase model

--- schemas.py
+++ schemas.py
@@ -2,3 +2,14 @@
 from datetime import datetime
 
 from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    """Created the UserBase model
+
+    Args:
+        BaseModel (Class): The parent class
+    """
+    email: str
+    name: str
+    phone_number: str
