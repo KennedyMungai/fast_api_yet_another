ecd0f0dfb39d9fafa725d78c1c581e196e19fdd6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-13 04:18:06 +0300

    Defined the create_db function

--- services.py
+++ services.py
@@ -1,2 +1,11 @@
 """A script meant to contain all the services logic"""
-from database import session_local
+from database import base, engine
+
+
+def create_db():
+    """The database creation method
+
+    Returns:
+        _type_: created the database
+    """
+    return base.metadata.create_all(bind=engine)
