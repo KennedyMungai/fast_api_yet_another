d6888a8ff608f87b6da28d37f994f44500086e0a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-13 03:42:42 +0300

    Created the UserModel class

--- models.py
+++ models.py
@@ -1,8 +1,21 @@
 """This file is going to hold all the models for the app"""
-import datetime
+from datetime import datetime
 
-import sqlalchemy
 import sqlalchemy.orm
 from passlib import hash
+from sqlalchemy import Column, DateTime, Integer, String
 
-from database import session_local
+from database import base, session_local
+
+
+class UserModel(base):
+    """The template for the User data
+
+    Args:
+        base (Class): The parent class for the UserModel
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password_hash = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow())
